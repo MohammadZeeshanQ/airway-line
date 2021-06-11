ca7a0bce4a6ecbefdc596bf223c0b2d93ea79c74
Zeeshan <mohammadzeshan610@yahoo.com>
2021-06-11 21:17:56 +0300

    Git Init and Components

---
+++ src/components/Index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingPage from './subComponent/Landing.js'
+
+export default function Index() {
+    return (
+        <div>
+            <LandingPage />
+        </div>
+    )
+}
---
+++ src/shared/NavBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography, Button, SwipeableDrawer, List, ListItem, Divider } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    root: {
+
+    },
+    wrapper: {
+
+    },
+    logoContainer: {
+
+    },
+    logo: {
+
+    },
+    tabContainer: {
+
+    },
+    tab: {
+
+    },
+    buttonContainer: {
+
+    },
+    outlineButton: {
+
+    },
+});
+
+export default function NavBar() {
+
+    const classes = useStyles();
+    const [drawer, setDrawer] = useState(false);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
